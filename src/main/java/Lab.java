a2798c120f76474232f9e63e241ad79a3032c21f
JitalSoni <Jits2096@gmail.com>
2023-02-28 21:24:59 +0000

    Unchecked Exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -12,8 +12,9 @@ public class Lab {
      * You could view the entire Exception family here: https://programming.guide/java/list-of-java-exceptions.html
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
+     * @throws Exception
      */
-    public void throwUncheckedException(){
-
+    public void throwUncheckedException() {
+        System.out.println(1/0);
     }
 }
